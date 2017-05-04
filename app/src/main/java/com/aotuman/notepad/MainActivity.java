419f3947d43eb5f28c82843064167abf6675304c
xufeng.zhang <xufeng.zhang@moji.com>
2017-05-04 11:52:39 +0800

    修改左抽屉，设置页面的布局样式

--- app/src/main/java/com/aotuman/notepad/MainActivity.java
+++ app/src/main/java/com/aotuman/notepad/MainActivity.java
@@ -1,6 +1,9 @@
 package com.aotuman.notepad;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
@@ -24,34 +27,36 @@ public class MainActivity extends Activity implements IMainView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intitActionBar();
         initView();
     }
 
     private void initView() {
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mLeftFragment = (LeftFragment) getSupportFragmentManager().findFragmentById(R.id.left_frame);
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,                  /* host Activity */
                 mDrawerLayout,         /* DrawerLayout object */
-                R.mipmap.ic_launcher,  /* nav drawer image to replace 'Up' caret */
+                R.mipmap.ic_launcher_round,  /* nav drawer image to replace 'Up' caret */
                 R.string.drawer_open,  /* "open drawer" description for accessibility */
                 R.string.drawer_close  /* "close drawer" description for accessibility */
         ) {
             public void onDrawerClosed(View view) {
-                getActionBar().setTitle("哈哈");
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
 
             public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle("嘻嘻");
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
         };
         mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
+    private void intitActionBar(){
+        ActionBar actionBar = getActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle(R.string.actionbar_title);
+        actionBar.setIcon(R.mipmap.ic_launcher_round);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 // The action bar home/up actionshould open or close the drawer.
