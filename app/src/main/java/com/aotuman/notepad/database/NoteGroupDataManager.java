2b9e876632f92213e6efcb564518d4bfbb053363
xufeng.zhang <xufeng.zhang@moji.com>
2017-05-10 14:05:11 +0800

    增加便分组的数据库

---
+++ app/src/main/java/com/aotuman/notepad/database/NoteGroupDataManager.java
@@ -0,0 +1,73 @@
+package com.aotuman.notepad.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.aotuman.notepad.entry.GroupInfo;
+import com.aotuman.notepad.entry.NotepadContentInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by aotuman on 2017/5/9.
+ */
+
+public class NoteGroupDataManager {
+    private static NoteGroupDataBaseHelp mNoteGroupDataBaseHelp = null;
+    private static NoteGroupDataManager instance;
+    private Context context;
+
+    public NoteGroupDataManager(Context context) {
+        this.context = context;
+        mNoteGroupDataBaseHelp = new NoteGroupDataBaseHelp(context);
+    }
+
+    public static NoteGroupDataManager getInstance(Context context) {
+        if (null == instance) {
+            instance = new NoteGroupDataManager(context);
+        }
+        return instance;
+    }
+
+    public void insertGroupInfo(GroupInfo info) {
+        String sql = "insert into groupinfo (name,count)values(?,?)";
+        SQLiteDatabase db = mNoteGroupDataBaseHelp.getWritableDatabase();
+        db.execSQL(sql, new Object[]{info.groupName,info.groupCount});
+        db.close();
+    }
+
+    public void updateGroupInfo(String name,long count) {
+        String sql = "update groupinfo set name = ?,count = ?";
+        SQLiteDatabase db = mNoteGroupDataBaseHelp.getWritableDatabase();
+        db.execSQL(sql, new Object[]{name,count});
+        db.close();
+    }
+
+    public void deleteNotepadInfo(String name) {
+        String sql = "delete from groupinfo where name = ?";
+        SQLiteDatabase db = mNoteGroupDataBaseHelp.getWritableDatabase();
+        db.execSQL(sql, new Object[]{name});
+        db.close();
+    }
+
+    public List<GroupInfo> findAllNotepad() {
+        List<GroupInfo> groupInfos = new ArrayList<>();
+        String sql = "select * from groupinfo";
+        SQLiteDatabase db = mNoteGroupDataBaseHelp.getWritableDatabase();
+        Cursor cursor;
+        cursor = db.rawQuery(sql, new String[]{});
+        if(null != cursor) {
+            while (cursor.moveToNext()) {
+                GroupInfo notepadContentInfo = new GroupInfo();
+                notepadContentInfo.groupName = cursor.getString(1);
+                notepadContentInfo.groupCount = cursor.getInt(2);
+                groupInfos.add(notepadContentInfo);
+            }
+            cursor.close();
+        }
+        db.close();
+        return groupInfos;
+}
+}
